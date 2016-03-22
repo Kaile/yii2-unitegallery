2ee72f4fe6be867db0e36dd91b4b879ead5fd620
Mikhail Kornilov <fix-06@yandex.ru>
2016-03-22 18:20:44 +0300

    Fix typo in asset

--- assets/GalleryAsset.php
+++ assets/GalleryAsset.php
@@ -6,7 +6,7 @@ use yii\web\AssetBundle;
 
 class GalleryAsset extends AssetBundle
 {
-	public $sourcePath = '@vendor/kaile/yii2-bootstrap-gallery';
+	public $sourcePath = '@vendor/kaile/yii2-unitegallery';
 
 	public $css = [
 		'css/unite-gallery.min.css',
