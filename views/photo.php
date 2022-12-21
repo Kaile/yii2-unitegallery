603118125f93e916d91cc5ee0054140faadb4f61
Mikhail Kornilov <kornilov@lib33.ru>
2022-12-21 11:21:12 +0300

    Fix remove ended dot before author

--- views/photo.php
+++ views/photo.php
@@ -12,7 +12,7 @@ $blockId = 'links-' . Widget::$autoIdPrefix . ++Widget::$counter;
 
 <div id="<?= $blockId ?>" class="bg-photo-items center-block links">
         <?php foreach ($items as $item): ?>
-            <?php $description = $item->description
+            <?php $description = trim($item->description, ' .')
                 . ($item->author ? Yii::t('bg', '. Автор: {author}', ['author' => $item->author]) : '')
                 . ($item->source ? Yii::t('bg', '. Источник: {source}', ['source' => $item->source]) : '')
             ?>
